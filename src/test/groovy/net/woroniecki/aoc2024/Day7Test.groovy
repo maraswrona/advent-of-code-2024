1121c3d42059d0d21a88b2fd2ec8780efbdf8d4f
maraswrona <woroniecki.marek@gmail.com>
2024-12-07 22:30:32 +0100

    day 7

---
+++ src/test/groovy/net/woroniecki/aoc2024/Day7Test.groovy
@@ -0,0 +1,41 @@
+package net.woroniecki.aoc2024
+
+import spock.lang.Specification
+
+class Day7Test extends Specification {
+
+    def "test"() {
+        given:
+        def input = """
+                        190: 10 19
+                        3267: 81 40 27
+                        83: 17 5
+                        156: 15 6
+                        7290: 6 8 6 15
+                        161011: 16 10 13
+                        192: 17 8 14
+                        21037: 9 7 18 13
+                        292: 11 6 16 20
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day7(input)
+
+        then:
+        day.part1() == 3749
+        day.part2() == 11387
+    }
+
+    def "puzzle"() {
+        given:
+        def input = Util.readFileToString("/aoc2024/day7.txt")
+
+        when:
+        def day = new Day7(input)
+
+        then:
+        day.part1() == 5540634308362
+        day.part2() == 472290821152397
+    }
+
+}
