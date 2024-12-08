ab4dd619f517afe5ad7f10001d5be06f8ff46845
maraswrona <woroniecki.marek@gmail.com>
2024-12-08 12:36:36 +0100

    day 8

---
+++ src/main/java/net/woroniecki/aoc2024/Day8.java
@@ -0,0 +1,115 @@
+package net.woroniecki.aoc2024;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import one.util.streamex.StreamEx;
+
+import java.util.*;
+
+public class Day8 {
+
+    final List<Antenna> antennas = new ArrayList<>();
+    final int width, height;
+    final Set<Point> antinodes = new HashSet<>();
+
+    @AllArgsConstructor
+    static class Antenna {
+        final Point p;
+        final char f;
+    }
+
+    @AllArgsConstructor
+    @Value
+    static class Point {
+        int x, y;
+
+        Point plus(Point p) {
+            return new Point(x + p.x, y + p.y);
+        }
+
+        Point minus(Point p) {
+            return new Point(x - p.x, y - p.y);
+        }
+
+        Point negate() {
+            return new Point(-x, -y);
+        }
+    }
+
+    public Day8(String input) {
+        String[] split = input.split("\n");
+        height = split.length;
+        width = split[0].trim().length();
+        for (int i = 0; i < split.length; i++) {
+            String line = split[i].trim();
+            char[] chars = line.trim().toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                char c = chars[j];
+                if (c != '.') {
+                    antennas.add(new Antenna(new Point(i, j), c));
+                }
+            }
+        }
+    }
+
+    public int part1() {
+        Map<Character, List<Antenna>> antennasPerFreq = StreamEx.of(antennas).groupingBy(a -> a.f);
+
+        antennasPerFreq.forEach((freq, ants) -> {
+            for (int i = 0; i < ants.size() - 1; i++) {
+                for (int j = i + 1; j < ants.size(); j++) {
+                    Antenna a1 = ants.get(i);
+                    Antenna a2 = ants.get(j);
+
+                    Point diff = a1.p.minus(a2.p);
+
+                    Point antinode1 = a1.p.plus(diff);
+                    Point antinode2 = a2.p.plus(diff.negate());
+
+                    if (checkCoords(antinode1)) {
+                        antinodes.add(antinode1);
+                    }
+
+                    if (checkCoords(antinode2)) {
+                        antinodes.add(antinode2);
+                    }
+                }
+            }
+        });
+
+        return antinodes.size();
+
+    }
+
+    public int part2() {
+        Map<Character, List<Antenna>> antennasPerFreq = StreamEx.of(antennas).groupingBy(a -> a.f);
+
+        antennasPerFreq.forEach((freq, ants) -> {
+            for (int i = 0; i < ants.size() - 1; i++) {
+                for (int j = i + 1; j < ants.size(); j++) {
+                    Point a1 = ants.get(i).p;
+                    Point a2 = ants.get(j).p;
+                    Point diff = a1.minus(a2);
+
+                    while (checkCoords(a1)) {
+                        antinodes.add(a1);
+                        a1 = a1.plus(diff);
+                    }
+
+                    while (checkCoords(a2)) {
+                        antinodes.add(a2);
+                        a2 = a2.plus(diff.negate());
+                    }
+                }
+            }
+        });
+
+        return antinodes.size();
+    }
+
+    private boolean checkCoords(Point p) {
+        return p.x >= 0 && p.x < height && p.y >= 0 && p.y < width;
+    }
+
+
+}
---
+++ src/test/groovy/net/woroniecki/aoc2024/Day8Test.groovy
@@ -0,0 +1,44 @@
+package net.woroniecki.aoc2024
+
+import spock.lang.Specification
+
+class Day8Test extends Specification {
+
+    def "test"() {
+        given:
+        def input = """
+                        ............
+                        ........0...
+                        .....0......
+                        .......0....
+                        ....0.......
+                        ......A.....
+                        ............
+                        ............
+                        ........A...
+                        .........A..
+                        ............
+                        ............
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day8(input)
+
+        then:
+        day.part1() == 14
+        day.part2() == 34
+    }
+
+    def "puzzle"() {
+        given:
+        def input = Util.readFileToString("/aoc2024/day8.txt")
+
+        when:
+        def day = new Day8(input)
+
+        then:
+        day.part1() == 247
+        day.part2() == 861
+    }
+
+}
