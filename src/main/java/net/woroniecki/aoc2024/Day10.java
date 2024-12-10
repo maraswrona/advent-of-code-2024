8960a23c9e46e1242e4c679bea5b01e968daddca
Marek Woroniecki <marek.woroniecki@kitopi.com>
2024-12-10 11:26:28 +0100

    day 10

---
+++ src/main/java/net/woroniecki/aoc2024/Day10.java
@@ -0,0 +1,18 @@
+package net.woroniecki.aoc2024;
+
+public class Day10 {
+
+    public Day10(String input) {
+
+    }
+
+    public void part1() {
+
+    }
+
+    public void part2() {
+
+    }
+
+
+}
---
+++ src/test/groovy/net/woroniecki/aoc2024/Day10Test.groovy
@@ -0,0 +1,40 @@
+package net.woroniecki.aoc2024
+
+import spock.lang.Specification
+
+class Day10Test extends Specification {
+
+    def "test"() {
+        given:
+        def input = """
+                        89010123
+                        78121874
+                        87430965
+                        96549874
+                        45678903
+                        32019012
+                        01329801
+                        10456732
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day10(input)
+
+        then:
+        day.part1() == 36
+        day.part2() == 0
+    }
+
+    def "puzzle"() {
+        given:
+        def input = Util.readFileToString("/aoc2024/day10.txt")
+
+        when:
+        def day = new Day10(input)
+
+        then:
+        day.part1() == 0
+        day.part2() == 0
+    }
+
+}
