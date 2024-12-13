d8ec151d5f70926357001414b931381a0b6ceb1e
maraswrona <woroniecki.marek@gmail.com>
2024-12-13 22:47:11 +0100

    day 12

--- src/main/java/net/woroniecki/aoc2024/Day12.java
+++ src/main/java/net/woroniecki/aoc2024/Day12.java
@@ -1,30 +1,44 @@
 package net.woroniecki.aoc2024;
 
-import java.util.LinkedList;
-import java.util.concurrent.ForkJoinPool;
-
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Day12 {
 
+    @Override
+    public String toString() {
+        return "Day 12";
+    }
+
     private final Plot[][] table;
+    private final int h, w;
+
+    private List<Region> regions = new ArrayList<>();
 
     public Day12(String input) {
         String[] lines = input.split("\n");
         table = new Plot[lines.length][];
         for (int i = 0; i < lines.length; i++) {
-            String line = lines[i];
+            String line = lines[i].trim();
             table[i] = new Plot[line.length()];
             for (int j = 0; j < line.length(); j++) {
-                table[i][j] = new Plot(line.charAt(j), false);
+                table[i][j] = new Plot(0, line.charAt(j), false);
             }
-
         }
+        this.h = table.length;
+        this.w = table[0].length;
+
+        this.captureRegions();
+        this.detectSides();
     }
 
     @AllArgsConstructor
     private static class Plot {
+        int regionId;
         char ch;
         boolean visited = false;
     }
@@ -36,36 +50,140 @@ public class Day12 {
         int x, y;
     }
 
+
+    @ToString
+    public static class Region {
+        static int nextId = 0;
+
+        static int nextId() {
+            return ++nextId;
+        }
+
+
+        char ch;
+        int id = nextId();
+        int area;
+        int perimeter;
+        int sides;
+        int maxX, maxY, minX, minY;
+    }
+
     public int part1() {
-        int total = 0;
+        captureRegions();
+        return regions.stream().mapToInt(r -> r.area * r.perimeter).sum();
+    }
+
+    public int part2() {
+        captureRegions();
+        detectSides();
+        return regions.stream().mapToInt(r -> r.area * r.sides).sum();
+    }
+
+    public Region region(char ch) {
+        return regions.stream().filter(r -> r.ch == ch).findFirst().orElseThrow();
+    }
+
+    public void detectSides() {
+        for (Region r : regions) {
+            // check cols
+            int segments = 0;
+            for (int x = r.minX; x <= r.maxX; x++) {
+                boolean aboveBlockStarted = false;
+                boolean belowBlockStarted = false;
+                for (int y = r.minY; y <= r.maxY; y++) {
+                    boolean currentCheck = table[x][y].regionId == r.id;
+                    boolean aboveCheck = x - 1 < 0 || table[x - 1][y].regionId != r.id;
+                    boolean belowCheck = x + 1 >= h || table[x + 1][y].regionId != r.id;
+                    if (currentCheck) {
+                        if (aboveCheck && !aboveBlockStarted) {
+                            aboveBlockStarted = true;
+                            segments++;
+                        }
+                        if (belowCheck && !belowBlockStarted) {
+                            belowBlockStarted = true;
+                            segments++;
+                        }
+                    }
+
+                    if (!currentCheck || !aboveCheck) {
+                        aboveBlockStarted = false;
+                    }
+
+                    if (!currentCheck || !belowCheck) {
+                        belowBlockStarted = false;
+                    }
+                }
+            }
+
+            // check rows
+            for (int y = r.minY; y <= r.maxY; y++) {
+                boolean leftBlockStarted = false;
+                boolean rightBlockStarted = false;
+                for (int x = r.minX; x <= r.maxX; x++) {
+                    boolean currentCheck = table[x][y].regionId == r.id;
+                    boolean leftCheck = y - 1 < 0 || table[x][y - 1].regionId != r.id;
+                    boolean rightCheck = y + 1 >= w || table[x][y + 1].regionId != r.id;
+                    if (currentCheck) {
+                        if (leftCheck && !leftBlockStarted) {
+                            leftBlockStarted = true;
+                            segments++;
+                        }
+                        if (rightCheck && !rightBlockStarted) {
+                            rightBlockStarted = true;
+                            segments++;
+                        }
+                    }
+
+                    if (!currentCheck || !leftCheck) {
+                        leftBlockStarted = false;
+                    }
+
+                    if (!currentCheck || !rightCheck) {
+                        rightBlockStarted = false;
+                    }
+                }
+            }
+            r.sides = segments;
+        }
+    }
+
+    public void captureRegions() {
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table[i].length; j++) {
                 Plot plot = table[i][j];
                 // . means already visited and counted
                 if (!plot.visited) {
-                    int cost = measureRegion(i, j);
-                    total += cost;
+                    Region region = detectRegion(i, j);
+                    regions.add(region);
                 }
             }
         }
-        return total;
     }
 
-    private int measureRegion(int x, int y) {
+    private Region detectRegion(int x, int y) {
 
+        Region region = new Region();
         LinkedList<Point> stack = new LinkedList<>();
         stack.push(new Point(x, y));
 
         int area = 0;
         int perimeter = 0;
+        int maxX = 0, maxY = 0, minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
         while (!stack.isEmpty()) {
             Point top = stack.pop();
             Plot plot = table[top.x][top.y];
             if (plot.visited) {
                 continue;
             }
+            region.ch = plot.ch;
+
+            maxX = Math.max(maxX, top.x);
+            maxY = Math.max(maxY, top.y);
+            minX = Math.min(minX, top.x);
+            minY = Math.min(minY, top.y);
 
             table[top.x][top.y].visited = true;
+            table[top.x][top.y].regionId = region.id;
             area++;
 
             for (int i = -1; i <= 1; i++) {
@@ -82,12 +200,15 @@ public class Day12 {
                 }
             }
         }
-        return area * perimeter;
+        region.maxX = maxX;
+        region.maxY = maxY;
+        region.minX = minX;
+        region.minY = minY;
+        region.area = area;
+        region.perimeter = perimeter;
+        return region;
     }
 
-    public int part2() {
-        return 0;
-    }
 
     private boolean checkCoords(int x, int y) {
         return x >= 0 && x < table.length && y >= 0 && y < table[x].length;
--- src/test/groovy/net/woroniecki/aoc2024/Day12Test.groovy
+++ src/test/groovy/net/woroniecki/aoc2024/Day12Test.groovy
@@ -1,12 +1,12 @@
 package net.woroniecki.aoc2024
 
-import net.woroniecki.DayTemplate
+
 import net.woroniecki.Util
 import spock.lang.Specification
 
 class Day12Test extends Specification {
 
-    def "test"() {
+    def "example 1"() {
         given:
         def input = """
                         AAAA
@@ -19,15 +19,113 @@ class Day12Test extends Specification {
         def day = new Day12(input)
 
         then:
+        with(day.region('A' as char)) {
+            area == 4
+            perimeter == 10
+            sides == 4
+        }
+
+        with(day.region('B' as char)) {
+            area == 4
+            perimeter == 8
+            sides == 4
+        }
+
+        with(day.region('C' as char)) {
+            area == 4
+            perimeter == 10
+            sides == 8
+        }
+
+        with(day.region('D' as char)) {
+            area == 1
+            perimeter == 4
+            sides == 4
+        }
+
+        with(day.region('E' as char)) {
+            area == 3
+            perimeter == 8
+            sides == 4
+        }
+
+        and:
         day.part1() == 140
-        //day.part2() == 0
+        day.part2() == 80
     }
 
-    def "example1"() {
+    def "example 2"() {
+        given:
+        def input = """
+                        OOOOO
+                        OXOXO
+                        OOOOO
+                        OXOXO
+                        OOOOO
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day12(input)
+
+        then:
+        with(day.region('O' as char)) {
+            area == 21
+            perimeter == 36
+        }
+
+        day.part1() == 772
+    }
+
+    def "example 3"() {
+        given:
+        def input = """
+                        RRRRIICCFF
+                        RRRRIICCCF
+                        VVRRRCCFFF
+                        VVRCCCJFFF
+                        VVVVCJJCFE
+                        VVIVCCJJEE
+                        VVIIICJJEE
+                        MIIIIIJJEE
+                        MIIISIJEEE
+                        MMMISSJEEE
+                        """.stripIndent().strip()
+
         expect:
-            new Day12("A").part1() == 4
-            new Day12("AA").part1() == 12
-            new Day12("AAA").part1() == 24
+        def day = new Day12(input).part1() == 1930
+    }
+
+    def "example 4"() {
+        given:
+        def input = """
+                        EEEEE
+                        EXXXX
+                        EEEEE
+                        EXXXX
+                        EEEEE
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day12(input)
+
+        then:
+        with(day.region('E' as char)) {
+            area == 17
+            sides == 12
+        }
+
+        day.part2() == 236
+    }
+
+    def "example 5"() {
+        expect:
+        new Day12("A").part1() == 4
+        new Day12("AA").part1() == 12
+        new Day12("AAA").part1() == 24
+
+        new Day12("A").part2() == 4
+        new Day12("AA").part2() == 8
+        new Day12("AAA").part2() == 12
     }
 
     def "puzzle"() {
@@ -38,8 +136,8 @@ class Day12Test extends Specification {
         def day = new Day12(input)
 
         then:
-        day.part1() == 0
-        day.part2() == 0
+        day.part1() == 1363484
+        day.part2() == 838988
     }
 
 }
