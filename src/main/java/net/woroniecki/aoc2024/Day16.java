0f1efb60d1b7a9d053dbd2cb972d9ea352671629
maraswrona <woroniecki.marek@gmail.com>
2024-12-16 20:39:36 +0100

    day 16

---
+++ src/main/java/net/woroniecki/aoc2024/Day16.java
@@ -0,0 +1,18 @@
+package net.woroniecki.aoc2024;
+
+public class Day16 {
+
+    public Day16(String input) {
+
+    }
+
+    public int part1() {
+        return 0;
+    }
+
+    public int part2() {
+        return 0;
+    }
+
+
+}
