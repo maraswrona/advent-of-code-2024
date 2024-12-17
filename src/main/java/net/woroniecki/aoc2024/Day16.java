98514a6ae3af93414835eecfbf73d9cb08976774
maraswrona <woroniecki.marek@gmail.com>
2024-12-17 14:33:45 +0100

    day 16

--- src/main/java/net/woroniecki/aoc2024/Day16.java
+++ src/main/java/net/woroniecki/aoc2024/Day16.java
@@ -1,18 +1,167 @@
 package net.woroniecki.aoc2024;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Day16 {
 
-    public Day16(String input) {
+    private final Block[][] grid;
+    private Node start, end;
+    AStar astar;
 
+    @AllArgsConstructor
+    public static class Block {
+        int x, y;
+        char ch;
+    }
+
+    public Day16(String input) {
+        String[] lines = input.split("\n");
+        grid = new Block[lines.length][];
+        for (int y = 0; y < lines.length; y++) {
+            char[] chars = lines[y].toCharArray();
+            grid[y] = new Block[chars.length];
+
+            for (int x = 0; x < grid[y].length; x++) {
+                grid[y][x] = new Block(x, y, chars[x]);
+
+                if (chars[x] == 'S') {
+                    start = new Node(x, y);
+                    grid[y][x] =  new Block(x, y, chars[x]);
+                }
+                if (chars[x] == 'E') {
+                    end = new Node(x, y);
+                    grid[y][x] = new Block(x, y, chars[x]);
+                }
+            }
+        }
+
+        astar = new AStar(start, end, grid);
     }
 
     public int part1() {
+        List<Node> path = new AStar(start, end, grid).reconstructPath(end);
+        System.out.println(path);
         return 0;
     }
 
     public int part2() {
         return 0;
     }
+
+
+    static class Node implements Comparable<Node> {
+        int x, y;
+        int g, h; // Koszty: g (start->tu), h (tu->cel)
+        Node parent;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int f() {
+            return g + h; // Łączny koszt
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.f(), other.f());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Node node = (Node) obj;
+            return x == node.x && y == node.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    @Getter
+    public static class AStar {
+        private final int[][] DIRECTIONS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // Ruchy: góra, prawo, dół, lewo
+        private final PriorityQueue<Node> openList = new PriorityQueue<>();
+        private final Set<Node> closedList = new HashSet<>();
+        private final Node start;
+        private final Node end;
+        private Node current;
+        private final Block[][] grid;
+
+        AStar(Node start, Node end, Block[][] grid) {
+            this.start = start;
+            this.end = end;
+            this.grid = grid;
+
+            this.start.g = 0;
+            this.start.h = heuristic(this.start, this.end);
+            openList.add(this.start);
+        }
+
+
+        public void step() {
+            if (!openList.isEmpty()) {
+                current = openList.poll();
+                if (current.equals(end)) {
+                    return;
+                }
+
+                closedList.add(current);
+
+                for (int[] direction : DIRECTIONS) {
+                    int newX = current.x + direction[0];
+                    int newY = current.y + direction[1];
+
+                    if (isValid(newX, newY)) {
+                        Node neighbor = new Node(newX, newY);
+                        if (closedList.contains(neighbor)) continue;
+
+                        int tentativeG = current.g + 1; // Koszt przejścia (1 na kratkę)
+                        if (tentativeG < neighbor.g || !openList.contains(neighbor)) {
+                            neighbor.g = tentativeG;
+                            neighbor.h = heuristic(neighbor, end);
+                            neighbor.parent = current;
+
+                            if (!openList.contains(neighbor)) {
+                                openList.add(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private int heuristic(Node a, Node b) {
+            return Math.abs(a.x - b.x) + Math.abs(a.y - b.y); // Heurystyka Manhattan
+        }
+
+        private boolean isValid(int x, int y) {
+            return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[y][x].ch == '.';
+        }
+
+        public List<Node> reconstructPath(Node node) {
+            List<Node> path = new ArrayList<>();
+            while (node != null) {
+                path.add(node);
+                node = node.parent;
+            }
+            Collections.reverse(path);
+            return path;
+        }
+    }
+
+    public Stream<Block> allBlocks() {
+        return Arrays.stream(grid).flatMap(Arrays::stream);
+    }
+
 
 
 }
---
+++ src/test/groovy/net/woroniecki/aoc2024/Day16Vis.java
@@ -0,0 +1,130 @@
+package net.woroniecki.aoc2024;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
+import net.woroniecki.Util;
+
+import java.io.IOException;
+
+public class Day16Vis {
+
+    public static void main(String[] args) throws IOException {
+
+        Day16 day = new Day16(Util.readFileToString("/aoc2024/day16.txt"));
+
+
+//        Day16 day = new Day16("###############\n" +
+//                "#.......#....E#\n" +
+//                "#.#.###.#.###.#\n" +
+//                "#.....#.#...#.#\n" +
+//                "#.###.#####.#.#\n" +
+//                "#.#.#.......#.#\n" +
+//                "#.#.#####.###.#\n" +
+//                "#...........#.#\n" +
+//                "###.#.#####.#.#\n" +
+//                "#...#.....#.#.#\n" +
+//                "#.#.#.###.#.#.#\n" +
+//                "#.....#...#.#.#\n" +
+//                "#.###.#.#.#.#.#\n" +
+//                "#S..#.....#...#\n" +
+//                "###############");
+
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                .setTerminalEmulatorFontConfiguration(SwingTerminalFontConfiguration.getDefaultOfSize(7))
+                .setInitialTerminalSize(new TerminalSize(150, 150));
+        Screen screen = null;
+
+
+        try {
+            screen = terminalFactory.createScreen();
+            screen.startScreen();
+
+            boolean auto = false;
+            boolean running = true;
+            int curr = 0;
+            while (running) {
+                KeyStroke keyStroke = screen.pollInput();
+                if (keyStroke != null) {
+                    switch (keyStroke.getKeyType()) {
+                        case ArrowRight -> day.astar.step();
+//                        case ArrowLeft -> day.move(Day15.Move.LEFT);
+//                        case ArrowUp -> day.move(Day15.Move.UP);
+//                        case ArrowDown -> day.move(Day15.Move.DOWN);
+                        case Character -> auto = (keyStroke.getCharacter() == ' ') != auto;
+                        case Escape -> running = false;
+                        case null, default -> {
+                        }
+                    }
+                }
+
+                if (auto) {
+                    for (int i = 0; i < 100; i++) {
+                        day.astar.step();
+                    }
+                }
+
+                drawGrid(screen, day);
+                Thread.sleep(10);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (screen != null) {
+                try {
+                    screen.stopScreen();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void drawGrid(Screen screen, Day16 day) throws IOException {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        screen.clear();
+
+//        textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
+//        for (int y = 0; y < day.grid.length; y++) {
+//            for (int x = 0; x < day.grid[y].length; x++) {
+//                textGraphics.putString(x, y, ".");
+//            }
+//        }
+
+        //textGraphics.setForegroundColor(TextColor.ANSI.WHITE_BRIGHT);
+
+//        for (int y = 0; y < day.h; y++) {
+//            textGraphics.putString(0, y, String.valueOf(y));
+//        }
+//
+//        for (int x = 0; x < day.w; x++) {
+//            textGraphics.putString(x, 0, String.valueOf(x));
+//        }
+
+//        textGraphics.putString(0, 0, String.valueOf(time));
+
+        day.allBlocks().forEach(r -> {
+            if (r.ch == '#') {
+                textGraphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
+            } else if (r.ch == '.') {
+                textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
+            }
+            textGraphics.putString(r.x, r.y, String.valueOf(r.ch));
+        });
+
+        textGraphics.setForegroundColor(TextColor.ANSI.RED_BRIGHT);
+        day.astar.reconstructPath(day.astar.getCurrent()).forEach(n -> {
+            textGraphics.putString(n.x, n.y, "*");
+        });
+
+        screen.refresh();
+    }
+
+}
+
+
