4edb0579a835886b87e588871c6c595527de88f7
maraswrona <woroniecki.marek@gmail.com>
2025-12-20 18:50:27 +0100

    day 11 wip

---
+++ src/test/groovy/net/woroniecki/aoc2025/Day11Test.groovy
@@ -0,0 +1,89 @@
+package net.woroniecki.aoc2025
+
+import lombok.extern.slf4j.Slf4j
+import net.woroniecki.Util
+import spock.lang.Specification
+
+@Slf4j
+class Day11Test extends Specification {
+
+    def "test1"() {
+        given:
+        def input = """
+                        aaa: you hhh
+                        you: bbb ccc
+                        bbb: ddd eee
+                        ccc: ddd eee fff
+                        ddd: ggg
+                        eee: out
+                        fff: out
+                        ggg: out
+                        hhh: ccc fff iii
+                        iii: out
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day11(input)
+
+        then:
+        day.part1("you", "out") == 5
+    }
+
+    def "test2"() {
+        given:
+        def input = """
+                        svr: aaa bbb
+                        aaa: fft
+                        fft: ccc
+                        bbb: tty
+                        tty: ccc
+                        ccc: ddd eee
+                        ddd: hub
+                        hub: fff
+                        eee: dac
+                        dac: fff
+                        fff: ggg hhh
+                        ggg: out
+                        hhh: out
+                        """.stripIndent().strip()
+
+        when:
+        def day = new Day11(input)
+
+        then:
+        day.part1("svr", "out") == 8
+//        day.part2() == 2
+
+        day.graph.dp()
+    }
+
+    def "puzzle"() {
+        given:
+        def input = Util.readFileToString("/aoc2025/day11.txt")
+
+        when:
+        def day = new Day11(input)
+
+        then:
+        day.part1("you", "out") == 688
+        day.part2() == 0
+    }
+
+    def "analysis()"() {
+
+        given:
+        def input = Util.readFileToString("/aoc2025/day11.txt")
+
+        when:
+        def day = new Day11(input)
+//        day.graph.analyzeGraphStructure()
+
+//        day.graph.part2()
+
+        day.graph.dp()
+
+        then:
+        1 == 1
+    }
+
+}
